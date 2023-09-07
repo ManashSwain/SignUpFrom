e869406f7ae50f42121f32e19ce0666496066218
ManashSwain <97601480+ManashSwain@users.noreply.github.com>
2023-09-07 21:05:22 +0530

    Added initial files

---
+++ index.js
@@ -0,0 +1,21 @@
+let nameField = document.getElementById('nameField');
+let title = document.getElementById('title');
+
+// Signin Function 
+
+ function signin() {
+    nameField.style.display='none';
+    title.innerHTML ="Sign-in";
+ }
+
+//  SignUp Function 
+
+
+ function signup() {
+    nameField.style.display='flex';
+    title.innerHTML ="Sign-up";
+    
+ }
+
+    
+
